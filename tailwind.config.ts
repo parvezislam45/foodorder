07b5998d39e83569dc205e5317f95738ac774f12
parvezislam45 <parvezislam45@gmail.com>
2025-07-09 21:30:23 +0600

    final commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,23 +13,22 @@ export default {
         foreground: "var(--foreground)",
       },
       animation: {
-        pulse: 'pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite',
-        'random-float': 'randomFloat 10s ease-in-out infinite',
+        pulse: "pulse 4s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        "random-float": "randomFloat 10s ease-in-out infinite",
       },
       keyframes: {
         pulse: {
-          '0%, 100%': { transform: 'scale(1)', opacity: 1 },
-          '50%': { transform: 'scale(1.5)', opacity: 0.7 },
+          "0%, 100%": { transform: "scale(1)", opacity: "1" },
+          "50%": { transform: "scale(1.5)", opacity: "0.7" },
         },
         randomFloat: {
-          '0%': { transform: 'translate(0, 0)' },
-          '25%': { transform: 'translate(50px, -50px)' },
-          '50%': { transform: 'translate(-50px, 50px)' },
-          '75%': { transform: 'translate(50px, 50px)' },
-          '100%': { transform: 'translate(-50px, -50px)' },
-        }
+          "0%": { transform: "translate(0, 0)" },
+          "25%": { transform: "translate(50px, -50px)" },
+          "50%": { transform: "translate(-50px, 50px)" },
+          "75%": { transform: "translate(50px, 50px)" },
+          "100%": { transform: "translate(-50px, -50px)" },
+        },
       },
-      
     },
   },
   plugins: [],
